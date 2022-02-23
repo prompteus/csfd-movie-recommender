d3e0ef32c76a55c02867d82afd8bbe087d6d9ebb
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-02-23 22:43:13 +0100

    add cli hello world

---
+++ src/recommend/main.py
@@ -0,0 +1,5 @@
+from recommend.utils import PROJ_ROOT
+
+
+def main():
+    print(f"hello world from project recommender with root at {PROJ_ROOT}.")
---
+++ src/recommend/utils.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+PROJ_ROOT = Path(__file__).parent.parent.parent
